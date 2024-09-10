9da3d0384255d93015f22b30498e00153af657aa
Jack Herrington <jherr@pobox.com>
2024-09-10 13:27:12 -0700

    Updating the chat API to the new AI API

--- 09-streaming/src/app/api/chat/route.ts
+++ 09-streaming/src/app/api/chat/route.ts
@@ -1,24 +1,17 @@
-import OpenAI from "openai";
-import { OpenAIStream, StreamingTextResponse } from "ai";
+import { openai } from "@ai-sdk/openai";
+import { streamText, convertToCoreMessages } from "ai";
 import { NextRequest } from "next/server";
 import { auth } from "@/auth";
 
 export const runtime = "edge";
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY!,
-});
-
 export const POST = auth(async function POST(req: NextRequest) {
   const { messages } = await req.json();
 
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    stream: true,
-    messages,
+  const result = await streamText({
+    model: openai("gpt-4-turbo"),
+    messages: convertToCoreMessages(messages),
   });
 
-  const stream = OpenAIStream(response);
-
-  return new StreamingTextResponse(stream);
+  return result.toDataStreamResponse();
 });
